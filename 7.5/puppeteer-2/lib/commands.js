82b872813389d908ae3eddc4215b31f429461453
alexbaskakau <123995421+alexbaskakau@users.noreply.github.com>
2023-10-06 11:49:10 +0100

    Add files via upload

--- 7.5/puppeteer-2/lib/commands.js
+++ 7.5/puppeteer-2/lib/commands.js
@@ -15,6 +15,35 @@ module.exports = {
       throw new Error(`Text is not available for selector: ${selector}`);
     }
   },
+  chooseSeat: async function (page, row, seat) {
+    try {
+      let selector = `main > section div:nth-child(${row}) > span:nth-child(${seat})`;
+      await page.waitForSelector(selector);
+      await page.click(selector);
+    } catch (error) {
+      throw new Error(`Selector is not clickable: ${selector}`); 
+    }
+  },
+  chooseDay : async function (page, day) {
+    try {
+      let date = `nav > a:nth-child(${day}) > span.page-nav__day-number`;
+      await page.waitForSelector(date);
+      await page.click(date);
+    } catch (error) {
+      throw new Error(`Selector is not clickable: ${date}`); 
+    }
+  },
+  
+  chooseMovie: async function (page, movie, time) {
+    try {
+      let hall = `body > main > section:nth-child(${movie}) > div:nth-child(${time}) > ul > li > a`;
+      await page.waitForSelector(hall);
+      await page.click(hall);
+    } catch (error) {
+      throw new Error(`Selector is not clickable: ${hall}`);
+    }
+  },
+  
   putText: async function (page, selector, text) {
     try {
       const inputField = await page.$(selector);
